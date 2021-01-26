3c8b3ede5a616ffdd4461c785ca026743fe5c40f
DESKTOP-1QOGJ0G\Hi <sadgood123@gmail.com>
2021-01-26 12:00:47 -0800

    no message

--- src/main/java/com/moon/web/IndexController.java
+++ src/main/java/com/moon/web/IndexController.java
@@ -2,17 +2,11 @@ package com.moon.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 	@GetMapping("")
 	public String index() {
 		return "index";
-	}
-	
-	@GetMapping("archi/mainprojects/{name}")
-	public String archiprojects(@PathVariable String name) {
-		return "archi/mainprojects/" + name;
 	}
 }
