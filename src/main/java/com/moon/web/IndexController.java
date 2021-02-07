0da76f594ead50c8265789cbb1f967118c964284
DESKTOP-1QOGJ0G\Hi <sadgood123@gmail.com>
2021-02-07 13:41:08 -0800

    aa

--- src/main/java/com/moon/web/IndexController.java
+++ src/main/java/com/moon/web/IndexController.java
@@ -18,13 +18,13 @@ public class IndexController {
 	
 	@GetMapping("")
 	public String index(Model model) {
-//		List<MoonImage> otherImages = moonImageRepository.findImagesByGroup(999);
-//		Base64.Encoder encoder = Base64.getEncoder();
-//		
-//		for (int i = 0; i < otherImages.size(); i++) {
-//			MoonImage moonimage = otherImages.get(i);
-//			model.addAttribute(moonimage.getImageName(), "data:image/png;base64," + encoder.encodeToString(moonimage.getImage()));
-//		}
+		List<MoonImage> otherImages = moonImageRepository.findImagesByGroup(999);
+		Base64.Encoder encoder = Base64.getEncoder();
+		
+		for (int i = 0; i < otherImages.size(); i++) {
+			MoonImage moonimage = otherImages.get(i);
+			model.addAttribute(moonimage.getImageName(), "data:image/png;base64," + encoder.encodeToString(moonimage.getImage()));
+		}
 		return "index";
 	}
 }
